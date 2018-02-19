5c3b89e0ec95e854974908a7f98548fa24c6978d
Juan Carlos Salazar <sacj.88@gmail.com>
2018-02-19 10:50:38 -0600

    redireccion a master

--- javascripts/pede_net.js
+++ javascripts/pede_net.js
@@ -517,7 +517,7 @@ $(document).ready(function () {
     var verbose = true;
 
     // 0 local, 1 producción, 2 desarrollo, 3 candidate
-    var ambiente = 3;
+    var ambiente = 1;
 
     // 0 nicho, 1 comunidad, 2 index
     var modulo = 1;
