41ef07582656ccd9a2e2d573da50cb6aa1c7c7cf
Juan Carlos Salazar <sacj.88@gmail.com>
2019-07-23 08:17:14 -0500

    correccion labels

--- javascripts/config.js
+++ javascripts/config.js
@@ -3,10 +3,6 @@ var url_front = "http://species.conabio.gob.mx/dbdev";
 var url_api = "http://species.conabio.gob.mx/api-db-dev";
 
 
-//var url_front = 'http://127.0.0.1:8000'
-//var url_api = 'http://127.0.0.1:8080'
-
-
 var url_nicho = url_front + "/geoportal_v0.1.html";
 var url_comunidad = url_front + "/comunidad_v0.1.html";
 var verbose = true;
