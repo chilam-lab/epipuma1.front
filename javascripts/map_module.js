2a7ea619c3c0532a4ec1a80dff20f3aa74d12505
Karla <karla@makingdevs.com>
2021-08-26 13:19:05 -0500

    236 Tomando cases training en lugar de tv

--- javascripts/map_module.js
+++ javascripts/map_module.js
@@ -2837,7 +2837,7 @@ var map_module = (function(url_geoserver, workspace, verbose, url_zacatuche) {
                               if(modifierFocus) {
                                 if (_data_sp_occ[i].target == true) {
                                     _data_sp_occ[i].occ = _data_sp_occ[i].cases_trainig
-                                    !modifierFocus ? sum_tv += parseInt(_data_sp_occ[i].tv) :sum_tv += parseInt(_data_sp_occ[i].cases_trainig)
+                                    sum_tv += parseInt(_data_sp_occ[i].cases_trainig)
                                     lalistadelosgradientes.push(_data_sp_occ[i])
                                 } else {
                                   _data_sp_occ[i].occ = 100
@@ -2846,7 +2846,7 @@ var map_module = (function(url_geoserver, workspace, verbose, url_zacatuche) {
                               } else {
                                  if(_data_sp_occ[i].tp == tp) {
                                   _data_sp_occ[i].occ = _data_sp_occ[i].tv
-                                  !modifierFocus ? sum_tv += parseInt(_data_sp_occ[i].tv) :sum_tv += parseInt(_data_sp_occ[i].cases_trainig)
+                                  sum_tv += parseInt(_data_sp_occ[i].cases_trainig)
                                   lalistadelosgradientes.push(_data_sp_occ[i])
                                 } else {
                                   _data_sp_occ[i].occ = 100
